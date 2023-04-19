be45ef3185e8fea5592c6744d7c8c0d4a3a6f4ed
benaziration <95994079+benaziration@users.noreply.github.com>
2023-04-19 11:09:01 +0600

    Add files via upload

---
+++ login_page/login_page/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  login_page
+//
+//  Created by Benazir Toleubekova on 17.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var username: UILabel!
+    
+    @IBOutlet weak var password: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.username.layer.cornerRadius = 20
+        self.password.layer.cornerRadius = 20
+        
+        // Do any additional setup after loading the view.
+    }
+
+   
+    }
+    
+
